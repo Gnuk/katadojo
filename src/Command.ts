98ea24d01d24bc7927f5d9e0c0645ccdbc8f13eb
Anthony Rey <arey@ippon.fr>
2022-07-13 12:44:17 +0200

    Improve kata (#3)

--- src/Command.ts
+++ src/Command.ts
@@ -1,16 +1,29 @@
 import { Command } from 'commander';
 
 import { Generator } from './Generator';
+import { Kata } from './Kata';
 import { Name } from './Name';
 
-const program = new Command();
+export const command = (show: (...messages: string[]) => unknown): Command => {
+  const program = new Command();
 
-program
-  .command('generate')
-  .description('generate a kata template')
-  .argument('<type>', 'template type')
-  .argument('<name>', 'template name')
-  .argument('<destination>', 'destination directory')
-  .action(async (type, name, destination) => await Generator.of({ kind: type, name: Name.of(name), pathname: destination }).generate());
+  program
+    .command('generate')
+    .description('generate a kata template')
+    .argument('<slug>', 'kata slug')
+    .argument('<name>', 'name as ASCII kebab-case')
+    .argument('<destination>', 'destination directory')
+    .action(
+      async (kata: string, name: string, destination: string) =>
+        await Generator.of({ kata: Kata.from(kata), name: Name.of(name), pathname: destination }).generate()
+    );
 
-export { program };
+  program
+    .command('slugs')
+    .description('list kata slugs')
+    .action(() => {
+      show(`Available slugs are: ${Kata.slugs().join(', ')}`);
+    });
+
+  return program;
+};
---
+++ src/Kata.ts
@@ -0,0 +1,56 @@
+const KINDS = ['ts-jest', 'ts-vitest', 'py', 'php'] as const;
+const SLUGS = [...KINDS, 'ts', 'ts-vite', 'typescript', 'python'] as const;
+
+export type Kind = typeof KINDS[number];
+export type Slug = typeof SLUGS[number];
+type CustomSlug = Exclude<Slug, Kind>;
+
+const CUSTOM_SLUG_TO_KIND: Record<CustomSlug, Kind> = {
+  ['ts']: 'ts-jest',
+  ['ts-vite']: 'ts-vitest',
+  ['typescript']: 'ts-jest',
+  ['python']: 'py',
+};
+
+const KIND_TO_REPOSITORY: Record<Kind, string> = {
+  ['ts-jest']: 'https://github.com/Gnuk/template-ts-kata',
+  ['ts-vitest']: 'https://github.com/Gnuk/template-ts-vite-kata',
+  ['py']: 'https://github.com/Gnuk/template-python-kata',
+  ['php']: 'https://github.com/Gnuk/template-php-kata',
+};
+
+const kindHasNot = (slug: string): boolean => KINDS.findIndex((kind) => kind === slug) === -1;
+
+const slugToKind = (slug: Slug): Kind => {
+  if (kindHasNot(slug)) {
+    return CUSTOM_SLUG_TO_KIND[slug as CustomSlug];
+  }
+  return slug as Kind;
+};
+
+const assertSlug = (slug: string): Slug => {
+  const trimSlug = slug.trim();
+  if (SLUGS.findIndex((s) => s === trimSlug) === -1) {
+    throw new Error(`The slug "${trimSlug}" is not in the slugs: ${SLUGS.join(', ')}`);
+  }
+  return trimSlug as Slug;
+};
+
+export class Kata {
+  private constructor(readonly kind: Kind, readonly repository: string) {}
+
+  static for(slug: Slug): Kata {
+    const kind = slugToKind(slug);
+    const repository = KIND_TO_REPOSITORY[kind];
+
+    return new Kata(kind, repository);
+  }
+
+  static slugs() {
+    return SLUGS;
+  }
+
+  static from(slug: string): Kata {
+    return Kata.for(assertSlug(slug));
+  }
+}
